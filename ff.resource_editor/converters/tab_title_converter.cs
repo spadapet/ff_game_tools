370dd3a6101fef27a8ea5317aa691e394a3ff971
Peter Spada <peterspa@microsoft.com>
2021-04-22 16:16:21 -0700

    Fix issue

--- ff.resource_editor/converters/tab_title_converter.cs
+++ ff.resource_editor/converters/tab_title_converter.cs
@@ -9,9 +9,9 @@ namespace ff.resource_editor.converters
     {
         public override object Convert(object[] values, Type target_type, object parameter, CultureInfo culture)
         {
-            edit_tab tab = (edit_tab)values[0];
-            string name = (string)values[1];
-            bool dirty = (bool)values[2];
+            edit_tab tab = values[0] as edit_tab;
+            string name = values[1] as string ?? string.Empty;
+            bool dirty = values[2] as bool? ?? false;
             string dirty_flag = dirty ? "*" : string.Empty;
 
             return $"{name}{dirty_flag}";
